0afc9064026c8ba8c5202b2eb89334e3a604a2a5
camyyskz <rm94770@estudante.fieb.edu.br>
2025-09-25 11:48:48 -0300

    consumo

---
+++ src/index.jsx
@@ -0,0 +1,32 @@
+// ferramenta de consumo de rotas ou endpoints do backend 
+import axios from "axios"
+// Hooks do react para controlar estados e renderização 
+import { useState, useEffect } from "react"
+const Produtos = () => {
+    // estado com lista de pizzas
+    const [pizzas, setPizzas] = useState([])
+    // Objeto com a lista de pizzas
+    useEffect(()=>{
+        // GET- busca os dados da rota ou endpoints 
+        // then- método assincrono, espera o retorno da requisição
+        // catch- tratamento de erros de requisição
+        axios.get("http://172.19.0.49/pizzariaoficial/api/v1/produto")
+        .then(response=>{
+            console.log(response.data.data)
+            setPizzas(response.data.data)
+        })
+    }, [])
+
+    // Iteração da lista de pizzas 
+    const listaPizzas = pizzas.map(pizza=><li key={pizza.id}>
+        {pizza.nome}</li>)
+    return (
+        <div>
+            <ul>
+                {listaPizzas}
+            </ul>
+        </div>
+    )
+}
+export default Produtos
+ 
